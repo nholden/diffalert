aec58d832691505c118e581aedf8257e1b9ed098
Nick Holden <nick@realhq.com>
2017-04-29 11:42:32 -0700

    Extract new trigger into own feature spec

--- spec/features/triggers_spec.rb
+++ spec/features/triggers_spec.rb
@@ -11,27 +11,6 @@ RSpec.describe "triggers" do
 
     Then { expect(page).to have_content user.email }
 
-    describe "creating a new Trigger" do
-      When { click_link 'Add trigger' }
-      When { fill_in 'Repository', with: 'sandbox' }
-      When { fill_in 'Branch', with: 'master' }
-      When { fill_in 'File path', with: 'README.md' }
-      When { fill_in 'Email', with: 'qwerty@slack.com' }
-      When { fill_in 'Message', with: 'README.md changed!' }
-      When { click_button 'Save trigger' }
-
-      Then { expect(page).to have_content 'New trigger created!' }
-      And { expect(page).to have_content 'sandbox' }
-      And { expect(page).to have_content 'master' }
-      And { expect(page).to have_content 'README.md' }
-
-      And { user.triggers.last.repository_name == 'sandbox' }
-      And { user.triggers.last.branch == 'master' }
-      And { user.triggers.last.modified_file == 'README.md' }
-      And { user.triggers.last.email == 'qwerty@slack.com' }
-      And { user.triggers.last.message == 'README.md changed!' }
-    end
-
     describe "deleting a Trigger" do
       Given!(:trigger) { FactoryGirl.create(:trigger, user: user) }
 
