558e4027a2d8527f5345f4e81efb36d4480e0722
Nick Holden <nick@realhq.com>
2017-05-03 20:43:24 -0700

    Beautify flash messages

--- app/controllers/concerns/requires_sign_in.rb
+++ app/controllers/concerns/requires_sign_in.rb
@@ -10,7 +10,7 @@ module RequiresSignIn
 
   def require_sign_in!
     if current_user.nil?
-      flash[:warning] = 'You must be logged in to view that page'
+      flash[:error] = 'You must be logged in to view that page'
       redirect_to new_session_path
     end
   end
