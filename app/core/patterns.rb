f3fb1a0ae0163358d5bb82ff7f98923ecdcc6b36
Nick Holden <nick@realhq.com>
2017-05-16 17:43:37 -0700

    Ensure a trigger has a valid email or Slack webhook URL

--- app/core/patterns.rb
+++ app/core/patterns.rb
@@ -1,5 +1,6 @@
 class Patterns
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_SLACK_WEBHOOK_URL_REGEX = /\Ahttps:\/\/hooks\.slack\.com\/.+\z/i
 
 end
--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -8,6 +8,16 @@ class Trigger < ApplicationRecord
                                        repository_name: event.repository_name) }
 
   validates :modified_file, :message, :branch, :repository_name, presence: true
-  validates :email, presence: true, format: { with: Patterns::VALID_EMAIL_REGEX }
+  validates :email, format: { with: Patterns::VALID_EMAIL_REGEX }, allow_blank: true
+  validates :slack_webhook_url, format: { with: Patterns::VALID_SLACK_WEBHOOK_URL_REGEX }, allow_blank: true
+  validate :must_have_email_or_slack_webhook_url
+
+  private
+
+  def must_have_email_or_slack_webhook_url
+    if email.blank? && slack_webhook_url.blank?
+      errors[:base] << 'A trigger must have an email and/or a Slack webhook URL'
+    end
+  end
 
 end
