8e665be4bcc6829a02e1da4faae7f46c30a40f4f
Nick Holden <nick@realhq.com>
2017-06-24 11:21:26 -0700

    Squelch "toplevel constant referenced" warning

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include ActiveSupport::Testing::TimeHelpers
+  config.include HtmlToPlainText
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
--- spec/workers/user_confirmation_email_worker_spec.rb
+++ spec/workers/user_confirmation_email_worker_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-include Premailer::HtmlToPlainText
 
 RSpec.describe UserConfirmationEmailWorker do
 
