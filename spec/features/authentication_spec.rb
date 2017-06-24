d02b79048dff031fa9814baa05b914b9d2c8f1be
Nick Holden <nick@realhq.com>
2017-06-23 17:11:24 -0700

    Move sign up feature specs into separate file

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -3,31 +3,6 @@ require 'support/authentication_helper'
 
 RSpec.describe "authentication" do
 
-  describe "sign-up" do
-    When { visit new_user_path }
-    When { fill_in 'Email', with: 'nick@realhq.com' }
-    When { fill_in 'Password', with: password }
-    When { fill_in 'Confirm password', with: password_confirmation }
-    When { click_button 'Sign up' }
-
-    context "with matching passwords" do
-      Given(:password) { 'password' }
-      Given(:password_confirmation) { 'password' }
-
-      Then { expect(page).to have_content 'You’re signed up!' }
-      And { User.last.email == 'nick@realhq.com' }
-      And { User.last.github_events_secret.present? }
-    end
-
-    context "without matching passwords" do
-      Given(:password) { 'password' }
-      Given(:password_confirmation) { 'passsword' }
-
-      Then { expect(page).to have_content 'This field doesn\'t match Password.' }
-      And { User.where(email: 'nick@realhq.com').empty? }
-    end
-  end
-
   describe "log in" do
     Given { FactoryGirl.create(:user, email: 'nick@nickholden.io', password: 'letmein', password_confirmation: 'letmein') }
 
