345e53df5304f0721587177e19c6adaf41c6d425
Nick Holden <nick@realhq.com>
2017-07-20 06:28:57 -0700

    Only show trigger file names (not file paths) on index page

--- app/decorators/trigger_decorator.rb
+++ app/decorators/trigger_decorator.rb
@@ -6,8 +6,12 @@ class TriggerDecorator < Draper::Decorator
     object.branch || 'All branches'
   end
 
-  def modified_file
-    object.modified_file || 'All files'
+  def modified_file_name
+    if object.modified_file
+      object.modified_file.split('/').last
+    else
+      'All files'
+    end
   end
 
   def last_alert_time_ago
